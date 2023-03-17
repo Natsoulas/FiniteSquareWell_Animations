782c59d9c33e3ce97f6c4660fd0b4738326db01a
Nicholas Natsoulas <nrn22@cornell.edu>
2023-03-17 20:26:48 +0100

    Final coding commit!! yay

--- codes/combined_wavefunction.py
+++ codes/combined_wavefunction.py
@@ -7,29 +7,19 @@ from scipy.integrate import quad
 a = 1
 ħ = 1  # Set ħ to 1 for simplicity, adjust as needed
 V0 = 25
-E0 = 1.7068  #EVEN: 1.7067 ODD: 6.7379 Ground state energy, odd case: 
-E1 = 14.7262  #EVEN: 14.7267 ODD: 16.848 First excited state energy
-k0 = np.sqrt(E0) # EVEN: 1.30640728718115 ODD: 2.59572725839985
-k1 = np.sqrt(E1) # EVEN: 1.97737199332852 ODD: 4.10463153035690
-gamma_0 = np.sqrt(V0-E0) # even: 4.82631329277327 odd: 4.27342953609861
-gamma_1 = np.sqrt(V0-E1) # even: 4.59238500128201 odd: 2.85517074795887
-
-#E0_odd =
-E1_odd = 16.848
-k1_odd = np.sqrt(E1_odd)
-#k0_odd
-#gamma_0_odd
-gamma_1_odd = np.sqrt(V0-E1_odd)
+E0 = 1.7068  #1st odd energy solution
+E1 = 6.737860956121000 #2nd odd state energy
+k0 = np.sqrt(E0) # 
+k1 = np.sqrt(E1) # 
+gamma_0 = np.sqrt(V0-E0) # even: 
+gamma_1 = np.sqrt(V0-E1) # even:
 
 A0 = np.sqrt(1/(((np.abs(np.exp(gamma_0*a)*np.cos(k0*a))**2))*np.exp(-2*gamma_0*a)/gamma_0 + a + np.sin(2*k0*a)/(2*k0)))
-A1 = np.sqrt(1/(((np.abs(np.exp(gamma_1*a)*np.cos(k1*a))**2))*np.exp(-2*gamma_1*a)/gamma_1 + a + np.sin(2*k1*a)/(2*k1)))
-B1 = np.sqrt(1/(((np.abs(np.exp(gamma_1_odd)*np.sin(k1_odd*a))**2))*np.exp(-2*gamma_1_odd*a)/gamma_1_odd + a - np.sin(2*k1_odd*a)/(2*k1_odd)))#ODD
-#B0 = 
-testF1 = A1*k1*np.sin(k1*a)*np.exp(gamma_1*a)/gamma_1
+B1 = np.sqrt(1/(((np.abs(np.exp(gamma_1*a)*np.sin(k1*a))**2))*np.exp(-2*gamma_1*a)/gamma_1 + a - np.sin(2*k1*a)/(2*k1)))
+
+
 F0 = A0*np.exp(gamma_0*a)*np.cos(k0*a)
-F1 = A1*np.exp(gamma_1*a)*np.cos(k1*a)
-#F0_odd
-F1_odd = -B1*k1_odd*np.sin(k1_odd*a)*np.exp(gamma_1_odd*a)
+F1 = -B1*np.sin(k1*a)*np.exp(gamma_1*a)
 
 c1 = 0.5
 c2 = np.sqrt(3)/2
@@ -45,6 +35,7 @@ def ψ0(x):
     result[cond2] = A0*np.cos(k0*x)  # Some expression for -a <= x <= a
     result[cond3] = F0*np.exp(-gamma_0*x)  # Some expression for x > a
     return result
+
 def ψ1(x):
     cond1 = x < -a
     cond2 = (-a <= x) & (x <= a)
@@ -52,19 +43,8 @@ def ψ1(x):
 
     result = np.zeros_like(x)
     result[cond1] = F1*np.exp(gamma_1*x)  # Some expression for x < -a
-    result[cond2] = A1*np.cos(k1*x)  # Some expression for -a <= x <= a
-    result[cond3] = F1*np.exp(-gamma_1*x)  # Some expression for x > a
-    return result
-
-def ψ1_odd(x):
-    cond1 = x < -a
-    cond2 = (-a <= x) & (x <= a)
-    cond3 = x > a
-
-    result = np.zeros_like(x)
-    result[cond1] = F1_odd*np.exp(gamma_1_odd*x)  # Some expression for x < -a
-    result[cond2] = B1*np.sin(k1_odd*x)  # Some expression for -a <= x <= a
-    result[cond3] = -F1_odd*np.exp(-gamma_1_odd*x)  # Some expression for x > a
+    result[cond2] = B1*np.sin(k1*x)  # Some expression for -a <= x <= a
+    result[cond3] = -F1*np.exp(-gamma_1*x)  # Some expression for x > a
     return result
 
 # Define the time-dependent probability distribution function
@@ -93,13 +73,13 @@ for i, x in enumerate(x_values):
         P1[i] = np.abs(F1*np.exp(gamma_1*x))**2
     elif -a <= x <= a:
         P0[i] = np.abs(A0*np.cos(k0*x))**2
-        P1[i] = np.abs(A1*np.cos(k1*x))**2
+        P1[i] = np.abs(B1*np.sin(k1*x))**2
     else:
         P0[i] = np.abs(F0*np.exp(-gamma_0*x))**2
-        P1[i] = np.abs(F1*np.exp(-gamma_1*x))**2
+        P1[i] = np.abs(-F1*np.exp(-gamma_1*x))**2
 
-plt.plot(x_values, P0, label='Ground State Distribution')
-plt.plot(x_values, P1, label='1st Excited State Distribution')
+plt.plot(x_values, P0, label='Ground State Distribution (even)')
+plt.plot(x_values, P1, label='1st Excited State Distribution (odd)')
 plt.xlabel('x')
 plt.ylabel('Probability')
 plt.legend()
@@ -111,20 +91,20 @@ for i, x in enumerate(x_values):
         P1[i] = F1*np.exp(gamma_1*x)
     elif -a <= x <= a:
         P0[i] = A0*np.cos(k0*x)
-        P1[i] = A1*np.cos(k1*x)
+        P1[i] = B1*np.sin(k1*x)
     else:
         P0[i] = F0*np.exp(-gamma_0*x)
-        P1[i] = F1*np.exp(-gamma_1*x)
+        P1[i] = -F1*np.exp(-gamma_1*x)
 
-plt.plot(x_values, P0, label='ψ0: Ground State')
-plt.plot(x_values, P1, label='ψ1: First Excited State')
+plt.plot(x_values, P0, label='ψ0: Ground State (even)')
+plt.plot(x_values, P1, label='ψ1: First Excited State (odd)')
 plt.xlabel('x')
-plt.ylabel('Even Wavefunctions')
+plt.ylabel('Wavefunctions')
 plt.legend()
 plt.show()
 
 
-integral, error = quad(probability_distribution_check, -10, 10)
+integral, error = quad(probability_distribution_ground, -10, 10)
 
 print(f"Integral of probability distribution at t0: {integral}")
 print(f"Error: {error}")
@@ -141,7 +121,7 @@ t = 0
 fig, ax = plt.subplots()
 ax.set_xlabel('x')
 ax.set_ylabel('P(x,t)')
-line, = ax.plot(x_values, probability_distribution(x_values, 0), label='Even Case Probability Distribution')
+line, = ax.plot(x_values, probability_distribution(x_values, 0), label='Probability Distribution')
 
 dt = 10/300
 # Update function for the animation
@@ -156,4 +136,4 @@ def update(frame):
 # Create and save the animation
 num_frames = 300
 ani = FuncAnimation(fig, update, frames=num_frames, blit=True, interval=1000/30)
-ani.save('distribution_animation_even.gif', writer='ffmpeg')
+ani.save('distribution_animation.gif', writer='ffmpeg')
--- codes/odd_wavefunction.py
+++ codes/odd_wavefunction.py
@@ -7,29 +7,19 @@ from scipy.integrate import quad
 a = 1
 ħ = 1  # Set ħ to 1 for simplicity, adjust as needed
 V0 = 25
-E0 = 1.7068  #EVEN: 1.7067 ODD: 6.7379 Ground state energy, odd case: 
-E1 = 14.7262  #EVEN: 14.7267 ODD: 16.848 First excited state energy
-k0 = np.sqrt(E0) # EVEN: 1.30640728718115 ODD: 2.59572725839985
-k1 = np.sqrt(E1) # EVEN: 1.97737199332852 ODD: 4.10463153035690
-gamma_0 = np.sqrt(V0-E0) # even: 4.82631329277327 odd: 4.27342953609861
-gamma_1 = np.sqrt(V0-E1) # even: 4.59238500128201 odd: 2.85517074795887
+E0 = 6.737860956121000 #1st odd energy solution
+E1 = 24.0717 #2nd odd state energy
+k0 = np.sqrt(E0) # 
+k1 = np.sqrt(E1) # 
+gamma_0 = np.sqrt(V0-E0) # even: 
+gamma_1 = np.sqrt(V0-E1) # even:
 
-#E0_odd =
-E1_odd = 16.848
-k1_odd = np.sqrt(E1_odd)
-#k0_odd
-#gamma_0_odd
-gamma_1_odd = np.sqrt(V0-E1_odd)
-
-A0 = np.sqrt(1/(((np.abs(np.exp(gamma_0*a)*np.cos(k0*a))**2))*np.exp(-2*gamma_0*a)/gamma_0 + a + np.sin(2*k0*a)/(2*k0)))
-A1 = np.sqrt(1/(((np.abs(np.exp(gamma_1*a)*np.cos(k1*a))**2))*np.exp(-2*gamma_1*a)/gamma_1 + a + np.sin(2*k1*a)/(2*k1)))
-B1 = np.sqrt(1/(((np.abs(np.exp(gamma_1_odd)*np.sin(k1_odd*a))**2))*np.exp(-2*gamma_1_odd*a)/gamma_1_odd + a - np.sin(2*k1_odd*a)/(2*k1_odd)))#ODD
+B0 = np.sqrt(1/(((np.abs(np.exp(gamma_0*a)*np.sin(k0*a))**2))*np.exp(-2*gamma_0*a)/gamma_0 + a - np.sin(2*k0*a)/(2*k0)))
+B1 = np.sqrt(1/(((np.abs(np.exp(gamma_1*a)*np.sin(k1*a))**2))*np.exp(-2*gamma_1*a)/gamma_1 + a - np.sin(2*k1*a)/(2*k1)))
 #B0 = 
-testF1 = A1*k1*np.sin(k1*a)*np.exp(gamma_1*a)/gamma_1
-F0 = A0*np.exp(gamma_0*a)*np.cos(k0*a)
-F1 = A1*np.exp(gamma_1*a)*np.cos(k1*a)
-#F0_odd
-F1_odd = -B1*k1_odd*np.sin(k1_odd*a)*np.exp(gamma_1_odd*a)
+
+F0 = -B0*np.sin(k0*a)*np.exp(gamma_0*a)
+F1 = -B1*np.sin(k1*a)*np.exp(gamma_1*a)
 
 c1 = 0.5
 c2 = np.sqrt(3)/2
@@ -41,10 +31,11 @@ def ψ0(x):
     cond3 = x > a
 
     result = np.zeros_like(x)
-    result[cond1] = F0*np.exp(gamma_0*x) # Some expression for x < -a
-    result[cond2] = A0*np.cos(k0*x)  # Some expression for -a <= x <= a
-    result[cond3] = F0*np.exp(-gamma_0*x)  # Some expression for x > a
+    result[cond1] = F0*np.exp(gamma_0*x)  # Some expression for x < -a
+    result[cond2] = B0*np.sin(k0*x)  # Some expression for -a <= x <= a
+    result[cond3] = -F0*np.exp(-gamma_0*x)  # Some expression for x > a
     return result
+
 def ψ1(x):
     cond1 = x < -a
     cond2 = (-a <= x) & (x <= a)
@@ -52,19 +43,8 @@ def ψ1(x):
 
     result = np.zeros_like(x)
     result[cond1] = F1*np.exp(gamma_1*x)  # Some expression for x < -a
-    result[cond2] = A1*np.cos(k1*x)  # Some expression for -a <= x <= a
-    result[cond3] = F1*np.exp(-gamma_1*x)  # Some expression for x > a
-    return result
-
-def ψ1_odd(x):
-    cond1 = x < -a
-    cond2 = (-a <= x) & (x <= a)
-    cond3 = x > a
-
-    result = np.zeros_like(x)
-    result[cond1] = F1_odd*np.exp(gamma_1_odd*x)  # Some expression for x < -a
-    result[cond2] = B1*np.sin(k1_odd*x)  # Some expression for -a <= x <= a
-    result[cond3] = -F1_odd*np.exp(-gamma_1_odd*x)  # Some expression for x > a
+    result[cond2] = B1*np.sin(k1*x)  # Some expression for -a <= x <= a
+    result[cond3] = -F1*np.exp(-gamma_1*x)  # Some expression for x > a
     return result
 
 # Define the time-dependent probability distribution function
@@ -92,11 +72,11 @@ for i, x in enumerate(x_values):
         P0[i] = np.abs(F0*np.exp(gamma_0*x))**2
         P1[i] = np.abs(F1*np.exp(gamma_1*x))**2
     elif -a <= x <= a:
-        P0[i] = np.abs(A0*np.cos(k0*x))**2
-        P1[i] = np.abs(A1*np.cos(k1*x))**2
+        P0[i] = np.abs(B0*np.sin(k0*x))**2
+        P1[i] = np.abs(B1*np.sin(k1*x))**2
     else:
-        P0[i] = np.abs(F0*np.exp(-gamma_0*x))**2
-        P1[i] = np.abs(F1*np.exp(-gamma_1*x))**2
+        P0[i] = np.abs(-F0*np.exp(-gamma_0*x))**2
+        P1[i] = np.abs(-F1*np.exp(-gamma_1*x))**2
 
 plt.plot(x_values, P0, label='Ground State Distribution')
 plt.plot(x_values, P1, label='1st Excited State Distribution')
@@ -110,21 +90,21 @@ for i, x in enumerate(x_values):
         P0[i] = F0*np.exp(gamma_0*x)
         P1[i] = F1*np.exp(gamma_1*x)
     elif -a <= x <= a:
-        P0[i] = A0*np.cos(k0*x)
-        P1[i] = A1*np.cos(k1*x)
+        P0[i] = B0*np.sin(k0*x)
+        P1[i] = B1*np.sin(k1*x)
     else:
-        P0[i] = F0*np.exp(-gamma_0*x)
-        P1[i] = F1*np.exp(-gamma_1*x)
+        P0[i] = -F0*np.exp(-gamma_0*x)
+        P1[i] = -F1*np.exp(-gamma_1*x)
 
 plt.plot(x_values, P0, label='ψ0: Ground State')
 plt.plot(x_values, P1, label='ψ1: First Excited State')
 plt.xlabel('x')
-plt.ylabel('Even Wavefunctions')
+plt.ylabel('Odd Wavefunctions')
 plt.legend()
 plt.show()
 
 
-integral, error = quad(probability_distribution_check, -10, 10)
+integral, error = quad(probability_distribution_ground, -10, 10)
 
 print(f"Integral of probability distribution at t0: {integral}")
 print(f"Error: {error}")
@@ -141,7 +121,7 @@ t = 0
 fig, ax = plt.subplots()
 ax.set_xlabel('x')
 ax.set_ylabel('P(x,t)')
-line, = ax.plot(x_values, probability_distribution(x_values, 0), label='Even Case Probability Distribution')
+line, = ax.plot(x_values, probability_distribution(x_values, 0), label='Odd Case Probability Distribution')
 
 dt = 10/300
 # Update function for the animation
@@ -156,4 +136,4 @@ def update(frame):
 # Create and save the animation
 num_frames = 300
 ani = FuncAnimation(fig, update, frames=num_frames, blit=True, interval=1000/30)
-ani.save('distribution_animation_even.gif', writer='ffmpeg')
+ani.save('distribution_animation_odd.mp4', writer='ffmpeg')
